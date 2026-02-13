bf8573e480b5eb4cc061ef9091d8e4443aeb151e
Sab44 <64696149+Sab44@users.noreply.github.com>
2026-02-13 12:32:55 +0100

    add new minor version for throughput unit conversion

--- homeassistant/components/libre_hardware_monitor/config_flow.py
+++ homeassistant/components/libre_hardware_monitor/config_flow.py
@@ -55,6 +55,7 @@ class LibreHardwareMonitorConfigFlow(ConfigFlow, domain=DOMAIN):
     """Handle a config flow for LibreHardwareMonitor."""
 
     VERSION = 2
+    MINOR_VERSION = 2
 
     def __init__(self) -> None:
         """Init config flow."""
