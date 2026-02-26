bd5cdf8c540ac5092622d94cfb95e69946b79ec4
Sab44 <64696149+Sab44@users.noreply.github.com>
2026-02-26 22:23:31 +0100

    add recorder platform to libre_hardware_monitor to provide custom equivalent units

--- tests/components/libre_hardware_monitor/test_init.py
+++ tests/components/libre_hardware_monitor/test_init.py
@@ -3,6 +3,9 @@
 import pytest
 
 from homeassistant.components.libre_hardware_monitor.const import DOMAIN
+from homeassistant.components.libre_hardware_monitor.recorder import (
+    async_custom_equivalent_units,
+)
 from homeassistant.components.recorder import Recorder
 from homeassistant.const import UnitOfDataRate
 from homeassistant.core import HomeAssistant
@@ -128,8 +131,10 @@ async def test_migration_to_sensor_device_classes(
     await init_integration(hass, legacy_config_entry_v2_1)
 
     entity_entry = entity_registry.async_get(f"sensor.{object_id}")
-
     assert entity_entry.unit_of_measurement == UnitOfDataRate.KILOBYTES_PER_SECOND
+
+    custom_equivalent_units = async_custom_equivalent_units(hass)
+    assert {f"sensor.{object_id}": {"KB/s": "kB/s"}} == custom_equivalent_units
 
     updated_config_entry = hass.config_entries.async_get_entry(
         legacy_config_entry_v2_1.entry_id
